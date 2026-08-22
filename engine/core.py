8441527b0fc1ef264fa7cb9302c9d52804973a03
TheServerBorn <dcovey34@gmail.com>
2026-08-22 00:09:33 -0500

    Add core engine interface

---
+++ engine/core.py
@@ -0,0 +1,28 @@
+from engine.games import load_games
+from engine.launcher import launch_program
+from engine.system_info import (
+    get_cpu_name,
+    get_memory_gb,
+    get_storage_info,
+    get_os_name,
+)
+
+
+class J29Engine:
+    def get_games(self):
+        return load_games()
+
+    def launch_game(self, program_path):
+        return launch_program(program_path)
+
+    def get_system_info(self):
+        storage = get_storage_info()
+
+        return {
+            "cpu": get_cpu_name(),
+            "memory_gb": get_memory_gb(),
+            "os_name": get_os_name(),
+            "system_drive": storage["system_drive"],
+            "total_gb": storage["total_gb"],
+            "free_gb": storage["free_gb"],
+        }
