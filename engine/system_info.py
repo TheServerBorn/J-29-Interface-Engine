3426d11ab7cb15c9e0732c08a2816f5fecd45230
TheServerBorn <dcovey34@gmail.com>
2026-08-21 23:04:43 -0500

    Add storage info to engine

--- engine/system_info.py
+++ engine/system_info.py
@@ -1,7 +1,8 @@
 import ctypes
 import platform
 import winreg
-
+import os
+import shutil
 
 def get_cpu_name():
     try:
@@ -38,3 +39,14 @@ def get_memory_gb():
     ctypes.windll.kernel32.GlobalMemoryStatusEx(ctypes.byref(memory_status))
 
     return round(memory_status.ullTotalPhys / (1024 ** 3))
+
+def get_storage_info():
+    system_drive = os.environ.get("SystemDrive", "C:") + "\\"
+
+    total, used, free = shutil.disk_usage(system_drive)
+
+    return {
+        "system_drive": system_drive,
+        "total_gb": round(total / (1024 ** 3)),
+        "free_gb": round(free / (1024 ** 3)),
+    }
