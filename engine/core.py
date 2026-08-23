4d1d62ff6cd6c8ba5908235a1d763df5a7b9656c
TheServerBorn <dcovey34@gmail.com>
2026-08-23 12:20:49 -0500

    Add favorite game support

--- engine/core.py
+++ engine/core.py
@@ -1,5 +1,6 @@
 from engine.theme import load_theme
 from engine.games import load_games
+from engine.game_state import load_game_state, save_game_state
 from engine.launcher import launch_program
 from engine.config import load_identity, load_settings
 from engine.system_info import (
@@ -13,6 +14,32 @@ from engine.system_info import (
 class J29Engine:
     def get_games(self):
         return load_games()
+
+    def get_favorite_games(self):
+        state = load_game_state()
+        favorite_ids = state.get("favorites", [])
+
+        return [
+            game
+            for game in self.get_games()
+            if game["id"] in favorite_ids
+        ]
+
+    def toggle_favorite(self, game_id):
+        state = load_game_state()
+        favorites = state.get("favorites", [])
+
+        if game_id in favorites:
+            favorites.remove(game_id)
+            is_favorite = False
+        else:
+            favorites.append(game_id)
+            is_favorite = True
+
+        state["favorites"] = favorites
+        save_game_state(state)
+
+        return is_favorite
 
     def launch_game(self, program_path):
         return launch_program(program_path)
